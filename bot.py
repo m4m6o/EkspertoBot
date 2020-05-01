bd7fbf432c708e00b0f199c64e75e04b73d0d5fe
m4m6o <45889084+m4m6o@users.noreply.github.com>
2020-05-01 12:15:39 +0600

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,98 @@
+#
+# here is main logic of bot
+# check how it works: @EkspertoBot
+# 
+
+import config
+from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
+from telegram.ext import Updater, MessageHandler, CommandHandler Filters
+
+# pos & neg callbacks
+CALLBACK_GOOD = '5'
+CALLBACK_BAD  = '2'
+# stickers
+GOOD_STICKER  = 'CAACAgIAAxkBAAJa4F6ruKEnj8ruZNU-aHOi4VGEVaOXAAI-AAN-aWUTI6JPEmql0IQZBA'
+BAD_STICKER   = 'CAACAgIAAxkBAAJa3l6ruJXqg3yW0gQLtmABjwnwh0KOAAJUAAN-aWUTDB6aKeV3oxkZBA'
+
+# create keyboard
+def generate_keyboard():
+	keyboard = [
+		[InlineKeyboardButton("5", callback_data=CALLBACK_GOOD),
+		 InlineKeyboardButton("2", callback_data=CALLBACK_BAD)]
+	]  # [[button1, button2]]
+	return InlineKeyboardMarkup(keyboard)
+
+# keyboard.onClickListener
+def keyboard_regulate(update: Update, context):
+	# get last callback
+	query = update.callback_query
+	current_callback = query.data
+
+	chat_id1 = update.effective_message.chat_id
+
+	# delete keyboard
+	query.edit_message_text(
+		text=update.effective_message.text
+	)
+
+	# 5
+	if current_callback == CALLBACK_GOOD:
+		context.bot.send_message(
+			chat_id = chat_id1,
+			text    = "nu molodec"
+		)
+		context.bot.send_message(
+			chat_id = chat_id1,
+			sticker = GOOD_STICKER	
+		)
+	# 2
+	elif current_callback == CALLBACK_BAD:
+		context.bot.send_message(
+			chat_id = chat_id1,
+			text    = "клоун"
+		)
+		context.bot.send_message(
+			chat_id = chat_id1,
+			sticker = BAD_STICKER
+		)
+
+# echo bot
+def hello(update: Update, context):
+	context.bot.send_message(
+		chat_id = update.effective_message.chat_id,
+		text    = update.effective_message.text
+	)
+
+# /start
+def start(update: Update, context):
+	user_name = update.effective_user.first_name
+	context.bot.send_message(
+		chat_id      = update.effective_message.chat_id,
+		text         = f"Привет, {user_name}!/nЧе как?",
+		reply_markup = generate_keyboard()
+	)
+
+# bot init
+def main():
+	# token.bind
+	my_update = Update(
+		token       = config.TOKEN,
+		base_url    = config.PROXI,
+		use_context = True
+	)
+	# handlers
+	keyboard_handler = CallbackQueryHandler(callback=keyboard_regulate, pass_chat_data = True)
+	msg_handler      = MessageHandler(Filters.all, hello)
+	start_handler    = CommandHandler("start", start)
+	# connect handlers to dispatcher 
+	my_update.dispatcher.add_handler(keyboard_handler)
+	my_update.dispatcher.add_handler(start_handler)
+	my_update.dispatcher.add_handler(msg_handler)
+	# Polling  - bot always handles to server and checks inf if changes
+	# WebHooks - server handle to bot if have changes
+	msg_update.start_polling()
+	msg_update.idle()
+
+
+if __name__ == "__main__":
+	main()
---
+++ config.py
@@ -0,0 +1,6 @@
+#
+# here is basic const data
+# check how it works: @EkspertoBot
+# 
+TOKEN = "831830281:AAGedl5SDsdBYfP85UT979lIXs3rLVTR-bs"
+PROXI = "https://telegg.ru/orig/bot"
